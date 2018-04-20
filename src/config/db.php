30b76292d03b0a14578601764eefc9d46ae2f0a6
Aldo Choque <achoque1400@gmail.com>
2018-04-19 23:50:23 -0500

    conexion pdo bd-slim

---
+++ src/config/db.php
@@ -0,0 +1,18 @@
+<?php
+class db{
+    private $host='localhost';
+    private $usuario='root';
+    private $password='';
+    private $base='misclientes';
+
+    //conectar BD
+
+    public function conectar(){
+        $conexion_mysql="mysql:host=$this->host;dbname=$this->base";
+        $conexionBD=new PDO($conexion_mysq,$this->usuario,$this->password);
+        $conexionBD->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        //esta line arregla la codificacion de caracteres UTF-8 
+        $conexionBD->exec("set names UTF8");
+        return $conexionBD;
+    }
+}
